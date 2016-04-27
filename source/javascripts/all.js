63e7ccdc90fa4af833a0cc5b70fc519f4f9ac65c
Guillaume Caulier <cauliergui@gmail.com>
2016-04-27 12:25:06 +0200

    Letter parallax

---
+++ source/javascripts/all.js
@@ -0,0 +1,28 @@
+//= require jquery-2.1.3.min
+//= require jquery.easing.1.3
+//= require jquery.parallax-scroll
+//= require plugin/jquery.easing
+//= require jquery-ui.min
+//= require bootstrap.min
+
+//= require plugin/jquery.flexslider
+//= require plugin/background-check.min
+//= require plugin/jquery.fitvids
+//= require plugin/jquery.viewportchecker
+//= require plugin/jquery.stellar.min
+//= require plugin/wow.min
+//= require plugin/jquery.colorbox-min
+//= require plugin/owl.carousel.min
+//= require plugin/isotope.pkgd.min
+//= require plugin/masonry.pkgd.min
+//= require plugin/imagesloaded.pkgd.min
+//= require plugin/jPushMenu
+//= require plugin/jquery.fs.tipper.min
+//= require plugin/mediaelement-and-player.min
+//= require plugin/jquery.validate.min
+//= require theme
+//= require navigation
+//= require plugin/jquery.singlePageNav
+//= require contact-form
+
+
